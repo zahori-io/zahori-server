cf79268719a002859eba787b854fe4304b6fddd7
David Vazquez <davidvazqueznovella@gmail.com>
2021-07-29 09:13:02 +0200

    Update browser versions: Chrome 91, Firefox 90, Safari 14, Edge 91, Opera 77

---
+++ database/sql_versions/V0.1.1.3__update_browser_versions.sql
@@ -0,0 +1,6 @@
+UPDATE public.browsers SET default_version = '91.0' WHERE browser_name = 'chrome';
+UPDATE public.browsers SET default_version = '90.0' WHERE browser_name = 'firefox';
+UPDATE public.browsers SET default_version = '91.0' WHERE browser_name = 'edge';
+UPDATE public.browsers SET default_version = '14.0' WHERE browser_name = 'safari';
+UPDATE public.browsers SET default_version = '77.0' WHERE browser_name = 'opera';
+
