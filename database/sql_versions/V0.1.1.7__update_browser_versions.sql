437b1c86b7e6041c7dfbc10019f230576edb1061
David Vazquez <davidvazqueznovella@gmail.com>
2022-04-05 13:38:36 +0200

    Update browser versions: Chrome 99, Firefox 98, Safari 15, Edge 99, Opera 85

---
+++ database/sql_versions/V0.1.1.7__update_browser_versions.sql
@@ -0,0 +1,6 @@
+UPDATE public.browsers SET default_version = '99.0' WHERE browser_name = 'chrome';
+UPDATE public.browsers SET default_version = '98.0' WHERE browser_name = 'firefox';
+UPDATE public.browsers SET default_version = '99.0' WHERE browser_name = 'edge';
+UPDATE public.browsers SET default_version = '15.0' WHERE browser_name = 'safari';
+UPDATE public.browsers SET default_version = '85.0' WHERE browser_name = 'opera';
+
