ce2e7b6460abeea7cc683103ba614a497b334be4
David Vazquez <davidvazqueznovella@gmail.com>
2023-01-12 12:23:48 +0100

    Update zahori-server version to 0.1.15

---
+++ database/sql_versions/V0.1.15.sql
@@ -0,0 +1,7 @@
+
+---------------- UPDATE BROWSER VERSIONS ----------------
+UPDATE public.browsers SET default_version = '108.0' WHERE browser_name = 'chrome';
+UPDATE public.browsers SET default_version = '108.0' WHERE browser_name = 'edge';
+UPDATE public.browsers SET default_version = '108.0' WHERE browser_name = 'firefox';
+UPDATE public.browsers SET default_version = '94.0' WHERE browser_name = 'opera';
+UPDATE public.browsers SET default_version = '15.0' WHERE browser_name = 'safari';
