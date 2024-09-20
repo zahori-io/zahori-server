0b6f8038db61cc1b70f843884704ad20e8634e24
David Vazquez <davidvazqueznovella@gmail.com>
2024-09-20 09:51:38 +0200

    Update browser versions: Chrome 128, Edge 124, Firefox 125, Opera 109, Safari 15

---
+++ database/sql_versions/V0.1.20__update_browser_versions.sql
@@ -0,0 +1,5 @@
+UPDATE public.browsers SET default_version = '128.0' WHERE browser_name = 'chrome';
+UPDATE public.browsers SET default_version = '124.0' WHERE browser_name = 'edge';
+UPDATE public.browsers SET default_version = '125.0' WHERE browser_name = 'firefox';
+UPDATE public.browsers SET default_version = '109.0' WHERE browser_name = 'opera';
+UPDATE public.browsers SET default_version = '15.0' WHERE browser_name = 'safari';
